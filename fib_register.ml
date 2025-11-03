b4c031c74266edfb1ab84d6fd06b1296f8331c12
Jack Dragoon <deulamco@gmail.com>
2025-11-03 17:11:22 +0700

    add branching

--- fib_register.ml
+++ fib_register.ml
@@ -1,12 +1,19 @@
 type opcode =
-  | Halt
-  | Mov of int * int     (* dst, src *)
-  | LoadI of int * int   (* dst, immediate *)
-  | Add of int * int * int  (* src1, src2, dst *)
+  | HALT
+  | MOV of int * int     (* dst, src *)
+  | LDI of int * int   (* dst, immediate *)
+
+  | ADD of int * int * int  (* src1, src2, dst *)
   | Sub of int * int * int
-  | Mul of int * int * int
+  | MUL of int * int * int
   | Div of int * int * int
-  | Print of int          (* reg *)
+
+  | LABEL of string       (* imm *)
+  | JMP of int            (* imm *)
+  | CMP of int * int * int(* src1, src2, dst *)
+  | JEQ of int * int      (* imm, src1 *)
+
+  | PRINT of int          (* reg *)
 
 type vm = {
   mutable registers : int array;
@@ -20,21 +27,25 @@ let create_vm program = {
   program;
 }
 
+let ax, bx, cx, dx, ex = 0,1,2,3,4
+
 let run vm =
   let len = Array.length vm.program in
+  Printf.printf "\nBEGIN (%d instructions)\n" len;
   let rec loop () =
     if vm.ip >= len then () else
+    Printf.printf "OPCODE: %d\n" vm.ip;
     match vm.program.(vm.ip) with
-    | Halt -> ()
-    | Mov (dst, src) ->
+    | HALT -> ()
+    | MOV (dst, src) ->
         vm.registers.(dst) <- vm.registers.(src);
         vm.ip <- vm.ip + 1;
         loop ()
-    | LoadI (dst, imm) ->
+    | LDI (dst, imm) ->
         vm.registers.(dst) <- imm;
         vm.ip <- vm.ip + 1;
         loop ()
-    | Add (src1, src2, dst) ->
+    | ADD (src1, src2, dst) ->
         vm.registers.(dst) <- vm.registers.(src1) + vm.registers.(src2);
         vm.ip <- vm.ip + 1;
         loop ()
@@ -42,7 +53,7 @@ let run vm =
         vm.registers.(dst) <- vm.registers.(src1) - vm.registers.(src2);
         vm.ip <- vm.ip + 1;
         loop ()
-    | Mul (src1, src2, dst) ->
+    | MUL (src1, src2, dst) ->
         vm.registers.(dst) <- vm.registers.(src1) * vm.registers.(src2);
         vm.ip <- vm.ip + 1;
         loop ()
@@ -51,22 +62,58 @@ let run vm =
         else vm.registers.(dst) <- vm.registers.(src1) / vm.registers.(src2);
         vm.ip <- vm.ip + 1;
         loop ()
-    | Print reg ->
-        Printf.printf "%d\n" vm.registers.(reg);
+    | PRINT reg ->
+        Printf.printf "PRINT: %d\n" vm.registers.(reg);
+        vm.ip <- vm.ip + 1;
+        loop ()
+    | LABEL name ->
+        Printf.printf "LABEL: %s @ %d\n" name vm.ip;
+        vm.ip <- vm.ip + 1;
+        loop ()
+    | JMP ip -> 
+        vm.ip <- ip;
+        loop ()
+    | JEQ (ip, reg) ->
+        if vm.registers.(reg) == 0 
+        then vm.ip <- ip
+        else vm.ip <- vm.ip + 1;
+        loop ()
+    | CMP (src1, src2, dst) -> 
+        let regs = vm.registers in
+        Printf.printf "CMP (%d, %d)" regs.(src1) regs.(src2);
+        regs.(dst) <- 
+          if regs.(src1) == regs.(src2) then 0 else 
+          if regs.(src1) > regs.(src2) then 1 else 
+          if regs.(src1) < regs.(src2) then 2 else 
+          failwith "Unsupported Method.";
+        Printf.printf " => %d\n" vm.registers.(dx);
         vm.ip <- vm.ip + 1;
         loop ()
   in
-  loop ()
-let r0, r1, r2, r3, r4 = 0,1,2,3,4
+  loop ();
+  Printf.printf "END PROGRAM\n"
+
 let () =
-  let program = [|
-    LoadI (r0, 10);     (* $0x0 = 10 *)
-    LoadI (r1, 20);     (* $0x1 = 20 *)
-    Add   (r0, r1, r2); (* $0x0 + $0x1 = $0x2  *)
-    LoadI (r3, 2);      (* $0x3 = 2 *)
-    Mul   (r2, r3, r4); (* $0x2 x $0x3 = $0x4 *)
-    Print  r4;          (* PRINT $0x4 *)
-    Halt;               (* Stop *)
-  |] in
-  let vm = create_vm program in
-  run vm
+  let test_ADD_MUL = [|
+    LDI (ax, 10);     (* $0x0 = 10 *)
+    LDI (bx, 20);     (* $0x1 = 20 *)
+    ADD (ax, bx, cx); (* exx0 + $0x1 = $0x2  *)
+    LDI (dx, 2);      (* $0x3 = 2 *)
+    MUL   (cx, dx, ex); (* $0x2 x $0x3 = $0x4 *)
+    PRINT  ex;          (* PRINT $0x4 *)
+    HALT;               (* Stop *)
+  |] in let vm = create_vm test_ADD_MUL in run vm;
+
+  let test_branching = [|
+    LDI (ax, 0);
+    LDI (bx, 1);
+    LDI (cx, 5);
+    LABEL("Entry");
+    ADD   (ax, bx, ax);
+    CMP   (ax, cx, dx);
+    PRINT (ax);
+    JEQ   (9, dx);  (* -> Exit *)
+    JMP   (3);      (* -> Entry *)
+    LABEL ("Exit");
+    HALT;
+  |] in let vm = create_vm test_branching in run vm;
