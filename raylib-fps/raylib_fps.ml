069c674e16530d556a243b8adec4122285c7ccdd
thetrung <deulamco@gmail.com>
2026-03-16 23:00:27 +0700

    sample camera + grid.

--- raylib-fps/raylib_fps.ml
+++ raylib-fps/raylib_fps.ml
@@ -4,13 +4,26 @@ let setup () =
   init_window 800 600 "Raylib FPS";
   set_target_fps 60
 
+let camera_setup () = 
+  Camera.create
+    (Vector3.create 6.0 6.0 6.0) (* camera position *)
+    (Vector3.create 0.0 2.0 0.0) (*looking at *)
+    (Vector3.create 0.0 1.0 0.0) (*up vector*)
+    45.0
+    CameraProjection.Perspective
+
 let loop () = 
+  let camera = camera_setup () in
   while not (window_should_close()) do
     match window_should_close() with
     | true -> close_window ();
     | false -> 
         begin_drawing();
         clear_background Color.raywhite;
+          begin_mode_3d camera;
+            (* something *)
+            draw_grid 10 1.0;
+          end_mode_3d ();
         end_drawing();
     done
   
